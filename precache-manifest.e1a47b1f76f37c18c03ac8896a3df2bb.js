7fe1dc9d88b76043a67b346ff39bca53d36103a6
JiMoon GIl <roadzmoon76@gmail.com>
2022-10-04 16:57:35 +0900

    Updates

---
+++ precache-manifest.e1a47b1f76f37c18c03ac8896a3df2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7999de9fe62dee5625518bfb0617d97d",
+    "url": "/habit-tracker-practice/index.html"
+  },
+  {
+    "revision": "e932c127eab24e1ccb2a",
+    "url": "/habit-tracker-practice/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit-tracker-practice/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit-tracker-practice/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e932c127eab24e1ccb2a",
+    "url": "/habit-tracker-practice/static/js/main.869fdb37.chunk.js"
+  },
+  {
+    "revision": "d31912b0c3b0003b0f00",
+    "url": "/habit-tracker-practice/static/js/runtime-main.1aedc2e7.js"
+  }
+]);
